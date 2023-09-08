03df7873d5f3c2788476e642cbf5cdb188e830d5
thiago.paiva <thiago.paiva@institutoiracema.org.br>
2023-09-08 11:04:50 -0300

    littlefs

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,13 @@
 #include <stdio.h>
+
 #include <string>
 
 #include "driver/gpio.h"
 #include "driver/uart.h"
+#include "esp_littlefs.h"
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "mp3_2x_NB.h"
-#include "vs1053.h"
-
-#define AUDIO_MISO 37
-#define AUDIO_MOSI 35
-#define AUDIO_CLK 36
-#define AUDIO_DC (gpio_num_t)41
-#define AUDIO_CS (gpio_num_t)40
-#define AUDIO_RESET (gpio_num_t)42
-#define DREQ (gpio_num_t)39
 
 void println(const char *message) {
   printf("%s\n", message);
@@ -25,10 +17,73 @@ void print(const char *message) {
   printf("%s", message);
 }
 
-VS1053_t *_audio = new VS1053_t(AUDIO_CLK,AUDIO_MOSI,AUDIO_MISO,AUDIO_CS,AUDIO_DC, DREQ, AUDIO_RESET);
-
 extern "C" {
 void app_main(void);
+}
+
+#include "esp_littlefs.h"
+#include "esp_log.h"
+#include "esp_system.h"
+
+static const char *TAG = "file_example";
+static const char *FILENAME = "/littlefs/hello.txt";  // Caminho para o arquivo no sistema de arquivos LittleFS
+static const char *DATA = "Hello world";              // Dados a serem escritos no arquivo
+
+esp_err_t init_littlefs() {
+  esp_err_t ret;
+
+  ESP_LOGI(TAG, "Initializing LittleFS");
+
+  esp_vfs_littlefs_conf_t conf = {
+      .base_path = "/littlefs",
+      .partition_label = "littlefs",
+      .format_if_mount_failed = true,
+      .dont_mount = false,
+  };
+
+  ret = esp_vfs_littlefs_register(&conf);
+
+  if (ret != ESP_OK) {
+    if (ret == ESP_FAIL) {
+      ESP_LOGE(TAG, "Failed to mount or format filesystem");
+    } else if (ret == ESP_ERR_NOT_FOUND) {
+      ESP_LOGE(TAG, "Failed to find LittleFS partition");
+    } else {
+      ESP_LOGE(TAG, "Failed to initialize LittleFS (%s)", esp_err_to_name(ret));
+    }
+  }
+
+  return ret;
+}
+
+void write_string_to_file() {
+  FILE *f = fopen(FILENAME, "w");
+  if (f == NULL) {
+    ESP_LOGE(TAG, "Failed to open file for writing");
+    return;
+  }
+
+  fprintf(f, "%s", DATA);
+  fclose(f);
+
+  ESP_LOGI(TAG, "String written to file: %s", DATA);
+}
+
+void read_string_from_file() {
+  FILE *f = fopen(FILENAME, "r");
+  if (f == NULL) {
+    ESP_LOGE(TAG, "Failed to open file for reading");
+    return;
+  }
+
+  char buffer[32];  // Suponha que a string tem no máximo 32 caracteres
+  if (fgets(buffer, sizeof(buffer), f) != NULL) {
+    ESP_LOGI(TAG, "Read string from file: %s", buffer);
+  } else {
+    ESP_LOGE(TAG, "Failed to read from file");
+  }
+
+  fclose(f);
 }
 
 void app_main() {
@@ -56,18 +111,17 @@ void app_main() {
   vTaskDelay(pdMS_TO_TICKS(20));
   gpio_set_level(GPIO_NUM_45, 0);
 
-  _audio->begin();
-  _audio->switchToMp3Mode();
-  _audio->setVolume(80);
+  init_littlefs();
 
+  write_string_to_file();
+  read_string_from_file();
 
   gpio_set_level(GPIO_NUM_45, 1);
   vTaskDelay(pdMS_TO_TICKS(20));
   gpio_set_level(GPIO_NUM_45, 0);
 
   while (1) {
-		_audio->playChunk((uint8_t *)casa_mp3, casa_mp3_len);
 
-    vTaskDelay(pdMS_TO_TICKS(10));
+    vTaskDelay(pdMS_TO_TICKS(1000));
   }
 }
